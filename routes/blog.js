dde75ae7eafdc0ec902a1d666ceb4d37571057b5
Nikol_Niven <ntopalusic@gmail.com>
2024-07-30 11:21:22 +0200

    handle async code errors

--- routes/blog.js
+++ routes/blog.js
@@ -66,9 +66,14 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts/:id", async function (req, res) {
+router.get("/posts/:id", async function (req, res, next) {
   const postId = req.params.id;
-  const objectId = new ObjectId(postId);
+  try {
+    const objectId = new ObjectId(postId);
+  } catch (error) {
+    return res.status(404).render("404");
+    //return next(error);
+  }
 
   const post = await dba.getDb().collection("posts").findOne({ _id: objectId });
 
