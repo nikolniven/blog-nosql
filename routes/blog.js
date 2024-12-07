3be78b97c183016f75dff5223a502eb98b69c245
Nikol_Niven <ntopalusic@gmail.com>
2024-12-07 09:31:31 +0100

    Checking which route is more efficiet in get request

--- routes/blog.js
+++ routes/blog.js
@@ -27,6 +27,34 @@ router.get("/new-post", async function (req, res) {
   res.render("create-post", { authors: authors });
 });
 
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await dba
+  .getDb()
+  .collection("authors")
+  .findOne({ _id: authorId });
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      authorId: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+  
+ // router.get("/posts:id", async function (req, res) {
+    // const singlePost = newObject(req.body.post);
+  // });
+  
+  // const result = await dba.getDb().collection("posts").insertOne(newPost);
+  // console.log(result);
+  // res.redirect("/posts");
+// });
+
 // router.get("/posts/:id", async function (req, res) {
 //   const postId = req.params.id;
 //   const objectId = new ObjectId(postId);
@@ -39,33 +67,7 @@ router.get("/new-post", async function (req, res) {
 //   res.render("post-detail", { singlePost: singlePost });
 // });
 
-router.post("/posts", async function (req, res) {
-  const authorId = new ObjectId(req.body.author);
-  const author = await dba
-    .getDb()
-    .collection("authors")
-    .findOne({ _id: authorId });
-  const newPost = {
-    title: req.body.title,
-    summary: req.body.summary,
-    body: req.body.content,
-    date: new Date(),
-    author: {
-      authorId: authorId,
-      name: author.name,
-      email: author.email,
-    },
-  };
-
-  router.get("/posts:id", async function (req, res) {
-    const singlePost = newObject(req.body.post);
-  });
-
-  const result = await dba.getDb().collection("posts").insertOne(newPost);
-  console.log(result);
-  res.redirect("/posts");
-});
-
+/////
 router.get("/posts/:id", async function (req, res) {
   const postId = req.params.id; // Extract the post ID from the URL
   try {
